bd0a2c4bc80e9cc88f086a1f8087428fd2bf28ab
maicondguerian <maicondguerian@gmail.com>
2023-01-27 01:31:11 -0300

    adicionando efeito blur no body ao clicar no input

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,13 +1,25 @@
 import Styles from './Search.module.css'
+import  { useState } from 'react';
 
-import { FiSearch } from "react-icons/fi";
- const Search =()=>{
+ const Search =({Icon})=>{
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    
+    const Modal =()=>{
+        return(
+            <>
+                { isModalVisible ? <div className={Styles.BlurBody} onClick={()=>setIsModalVisible(false)}></div> : null}
+            </>
+        )
+    }
+
     return(
         <>
             <div  className={Styles.inputBar}>
-                <input type="text" className={Styles.inputSearchBar} name="Search-bar" value="" placeholder='busque aqui seu produto'/>
-                <FiSearch className={Styles.IconSearch} onClick={true} />
+                <input type="text" className={Styles.inputSearchBar} name="Search-bar" value="" placeholder='busque aqui seu produto' onClick={()=>setIsModalVisible(true)} />
+                { Icon == null ? <></> : <Icon className={Styles.IconSearch} />}
+                <Modal/>
             </div>
+            
         </>
     )
  }
