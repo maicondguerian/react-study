09288023336f83e5d9acc3be139bced235f88cee
Anderson Babinski <andeerlb@gmail.com>
2023-02-01 20:35:13 -0300

    ajustes no modal de responsabilidades e exibicao

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,34 +1,18 @@
-import React, { useState } from "react";
-import style from './modal.css'
+import React from "react";
+import './modal.css';
 
-export default function Modal (){
-
-    const [modal, setModal] = useState(false)
-    const toggleModal = () =>{
-        setModal(!modal);
-    }
+export default function Modal ({ children, isOpen=false }) {
     return (
-    <>
-        <div className="teste"
-        onMouseEnter={toggleModal}>
-            <button type=""></button>
-        </div>
-        {modal &&(
-            <div className="Modal">
-            <div className="overlay">
-                <div className="content">
-                    <p>pra ver seus pedidos e ter uma experiência personalizada, acesse sua conta :)</p>
-                    <button type="">entrar</button>
-                    <button type="">cadastrar</button>
-                    <p>comprou na loja física ou no app da Ame? Acompanhar o pedido</p>
-                    
+        <>
+            {isOpen &&(
+                <div className="Modal">
+                    <div className="overlay">
+                        <div className="content">
+                           {children}
+                        </div>
+                    </div>
                 </div>
-            </div>
-            
-        </div>
-        )}
-        
-
-    </>
-)
+            )}
+        </>
+    )
 }
